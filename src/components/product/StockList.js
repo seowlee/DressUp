b9ceddf6fa028ce63a3d3a556c3c7058e0b4538f
seowooo <tegfsl@gmail.com>
2022-07-13 00:52:44 +0900

    FEAT: Mapping ProductList Components
    ProductPhotoList & RecommendationList & StockList in MainPage

--- src/components/product/StockList.js
+++ src/components/product/StockList.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Carousel from 'better-react-carousel';
+import React from 'react'
+import Carousel from 'better-react-carousel'
 
-const ProductPhotoList = () => {
+const StockList = () => {
   const arr = [
 		{id: 1, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 로즈', size: 'xs',
       url: ["https://picsum.photos/800/600?random=1",
@@ -22,39 +22,26 @@ const ProductPhotoList = () => {
       url: ["https://picsum.photos/800/600?random=1",
             "https://picsum.photos/800/600?random=2",
             "https://picsum.photos/800/600?random=1",
+            "https://picsum.photos/800/600?random=2"]},
+
+    {id: 4, name: 'bbb', price: '50,000원', count: '3', color: '단색', size: 'L',
+      url: ["https://picsum.photos/800/600?random=1",
+            "https://picsum.photos/800/600?random=2",
+            "https://picsum.photos/800/600?random=1",
             "https://picsum.photos/800/600?random=2"]}
 	];
-  // const product = arr.find(obj => {
-	// 	return obj.id === 1;
-	// });
-  // console.log(product.url);
-
-  // const productPhotos = product.url.map((url, idx) => {
-  //   console.log(url);
-  // });
 
   return (
-    <Carousel 
-		cols={1} 
-		rows={1} 
-		gap={10} 
-		showDots={true} 
-		dotColorActive="#646262" 
-		loop>
-      {arr[0].url.map((data)=>{
+    <Carousel cols={4} rows={1} gap={10} loop>
+      {arr.map((data)=>{
         return <Carousel.Item key={data.id}>
-        <img width="100%" src={data} />
+        <img width="100%" src={data.url[0]} />
+        <div>{data.name}</div>
+        <div>{data.price}</div>
       </Carousel.Item>
       })}
-      {/* 
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=2" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=3" />
-      </Carousel.Item>*/}
     </Carousel>
   );
 };
 
-export default ProductPhotoList;
+export default StockList;
