54a808b5d86fff6017fd4623a3cd3a8aa873e869
seowooo <tegfsl@gmail.com>
2022-07-13 11:16:40 +0900

    FEAT: Add a Button to delete product from ShoppingCartList in ShoppingCartPage.js

--- src/components/cart/ShoppingCartList.js
+++ src/components/cart/ShoppingCartList.js
@@ -1,61 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'better-react-carousel';
 import {
 	Grid,
 	Typography,
+	Button,
 	Divider,
 	} from '@mui/material';
-
+	
+function Comp2({ deleteHandeler, id }) {
+	return (
+	<div>
+		{id}
+		<input
+		type="button"
+		id="delete"
+		value="Delete"
+		onClick={deleteHandeler}
+		/>
+	</div>
+	);
+}
+const arr = [
+	{id: 1, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 로즈', size: 'xs',
+  url: ["https://picsum.photos/800/600?random=1",
+		"https://picsum.photos/800/600?random=2",
+		"https://picsum.photos/800/600?random=3",
+		"https://picsum.photos/800/600?random=4",
+		"https://picsum.photos/800/600?random=4",
+	  ]},
+	{id: 2, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 블루', size: 'xs',
+  url: ["https://picsum.photos/800/600?random=1",
+		"https://picsum.photos/800/600?random=2",
+		"https://picsum.photos/800/600?random=3",
+		"https://picsum.photos/800/600?random=4"]},
+	{id: 3, name: 'aaa', price: '1,000원', count: '2', color: '블랙', size: 's',
+  url: ["https://picsum.photos/800/600?random=5",
+		"https://picsum.photos/800/600?random=6",
+		"https://picsum.photos/800/600?random=7",
+		"https://picsum.photos/800/600?random=8"]}
+];
 const ShoppingCartList = () => {
-	const arr = [
-		{id: 1, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 로즈', size: 'xs',
-      url: ["https://picsum.photos/800/600?random=1",
-            "https://picsum.photos/800/600?random=2",
-            "https://picsum.photos/800/600?random=3",
-            "https://picsum.photos/800/600?random=4",
-            "https://picsum.photos/800/600?random=4",
-          ]},
-		{id: 2, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 블루', size: 'xs',
-      url: ["https://picsum.photos/800/600?random=1",
-            "https://picsum.photos/800/600?random=2",
-            "https://picsum.photos/800/600?random=3",
-            "https://picsum.photos/800/600?random=4"]},
-		{id: 3, name: 'aaa', price: '1,000원', count: '2', color: '블랙', size: 's',
-      url: ["https://picsum.photos/800/600?random=5",
-            "https://picsum.photos/800/600?random=6",
-            "https://picsum.photos/800/600?random=7",
-            "https://picsum.photos/800/600?random=8"]},
-		{id: 4, name: 'bbb', price: '50,000원', count: '3', color: '단색', size: 'L',
-      url: ["https://picsum.photos/800/600?random=1",
-            "https://picsum.photos/800/600?random=2",
-            "https://picsum.photos/800/600?random=1",
-            "https://picsum.photos/800/600?random=2"]},
-			{id: 1, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 로즈', size: 'xs',
-			url: ["https://picsum.photos/800/600?random=1",
-				  "https://picsum.photos/800/600?random=2",
-				  "https://picsum.photos/800/600?random=3",
-				  "https://picsum.photos/800/600?random=4",
-				  "https://picsum.photos/800/600?random=4",
-				]},
-			  {id: 2, name: '옥스퍼드 패널 원피스', price: '1,000,000원', count: '1', color: '라이트 블루', size: 'xs',
-			url: ["https://picsum.photos/800/600?random=1",
-				  "https://picsum.photos/800/600?random=2",
-				  "https://picsum.photos/800/600?random=3",
-				  "https://picsum.photos/800/600?random=4"]},
-			  {id: 3, name: 'aaa', price: '1,000원', count: '2', color: '블랙', size: 's',
-			url: ["https://picsum.photos/800/600?random=5",
-				  "https://picsum.photos/800/600?random=6",
-				  "https://picsum.photos/800/600?random=7",
-				  "https://picsum.photos/800/600?random=8"]},
-			  {id: 4, name: 'bbb', price: '50,000원', count: '3', color: '단색', size: 'L',
-			url: ["https://picsum.photos/800/600?random=1",
-				  "https://picsum.photos/800/600?random=2",
-				  "https://picsum.photos/800/600?random=1",
-				  "https://picsum.photos/800/600?random=2"]}
-	];
+	const [list, updateList] = useState(arr);
+	// const addHandeler = () => {
+	// 	const newId = nanoid();
+	// 	setIds((ids) => [...ids, newId]);
+	// };
+
+	const handleRemoveItem = (selectedId) => {
+		const id = selectedId;
+		console.log(id)
+		//  updateList(list.filter(data => data.id !== id));
+		const temp = [...list];
+		console.log(temp);
+		temp.splice(id-1, 1);
+		console.log(temp);
+		updateList(temp);
+		console.log(list)
+	};
   return (
 	<Carousel hideArrow={true} cols={1} rows={arr.length} gap={10} loop>
-      {arr.map((data)=>{
+      {list.map((data)=>{
         return <Carousel.Item key={data.id}>
 			<Grid 
 				backgroundColor="#E1DEDE" 
@@ -64,7 +68,7 @@ const ShoppingCartList = () => {
 				<Grid item xs={6}>
 					<img width="50%" src={data.url[0]} />
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={4}>
 					<Typography pt={3} pb={3} variant="h5" component="div" align='left'>
 						{data.name}
 					</Typography>
@@ -72,6 +76,14 @@ const ShoppingCartList = () => {
 						{data.price}
 					</Typography>
 				</Grid>
+				<Grid item xs={2}>
+					<Button onClick={()=> {handleRemoveItem(data.id)}}
+						style={{
+							backgroundColor: "#000000"
+						}}
+						variant="contained" size="large">삭제</Button>
+				</Grid>
+
 			</Grid>
       </Carousel.Item>
       })}
