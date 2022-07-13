8afb794b4b3bfd954250a687bd100890cdde8e8e
기영 <86389879+kwan9069@users.noreply.github.com>
2022-07-13 14:08:53 +0900

    Revert "Revert "기영 서버 추가 및 QR""

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,8 +1,51 @@
-import React from 'react';
+
 import styled from 'styled-components';
 import ResponsiveAppBar from '../components/appbar/ResponsiveAppBar';
 import ProductInfo from '../components/product/ProductInfo';
 import ShoppingCartButton from '../components/cart/ShoppingCartButton';
+import Axios from "axios"
+import React, {useState, useEffect} from 'react'
+import QRpage from './QRpage';
+import {Link,Route,BrowserRouter as Router} from 'react-router-dom'
+
+
+
+
+// const submitReview =() => {
+//   Axios.post('http://localhost:3001/apit/insert',{image_url: image_url, name: name, color: color, count: count,size: size,price: price})
+// .then(() => {
+// alert('successfully inserted')
+// })
+// }
+
+
+// const [item,itemlist]= useState('')
+
+// const MainPage = () => {
+//   const [image_url, image]=useState('')
+//   const [price_sym, price]=useState('')
+//   const [lists,viewlist]=useState([])
+//   useEffect(() => { Axios.get('http://localhost:3001/api/get').then((response)=>{
+//     console.log(response)
+//     viewlist(response.data)
+//   })},[])
+
+
+//   return (
+//     <div style={{ textAlign : 'center'}}>
+//       MainPage
+//       <br></br>
+//       <h1>DressUP!!</h1>
+//       <Link to='./QRpage'>
+//       <button>QR 구매</button>
+//       </Link>
+//       <button>QR 재고확인</button>
+//       {lists.map((val)=>{
+//        return <h1>Color: {val.color} | 
+//        Size: {val.size} </h1>})}
+//     </div>
+// )}
+
 
 const MainPageDiv = styled.div`
   // background-color: black;
@@ -29,3 +72,4 @@ const MainPage = () => {
 }
 
 export default MainPage;
+
