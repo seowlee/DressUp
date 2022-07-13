af9bad3df4c020d75ebf30efb197caa56a24eac0
prjustinakang <prjustinakang@gmail.com>
2022-07-13 13:34:45 +0900

    paymentfin page add

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,24 @@ import LogoPage from './pages/LogoPage';
 import MainPage from './pages/MainPage';
 import PaymentPage from './pages/PaymentPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
+import PaymentFinPage from './pages/PaymentfinPage';
 import { Button } from 'antd';
 import './App.css';
 // import Routes from "./routes/Routes";
 
 function App() {
-  return (
-    // <Routes />
-    <BrowserRouter>
-		  <Routes>
-			  <Route path="/" element={<LogoPage />}></Route>
-			  <Route path="/main" element={<MainPage />}></Route>
-			  <Route path="/payment" element={<PaymentPage />}></Route>
-			  <Route path="/shoppingcart" element={<ShoppingCartPage />}></Route>
-		  </Routes>
-	</BrowserRouter>
-  );
+	return (
+		// <Routes />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LogoPage />}></Route>
+				<Route path="/main" element={<MainPage />}></Route>
+				<Route path="/payment" element={<PaymentPage />}></Route>
+				<Route path="/shoppingcart" element={<ShoppingCartPage />}></Route>
+				<Route path="/paymentfin" element={<PaymentFinPage />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
